357a26b3f2902ec23c53e07ab3eb5bc925c1b05c
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-25 12:15:13 -0300

    Feat: Created Signin & Signup auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './models/users.model';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
+import { AuthGuard } from '@nestjs/passport';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userServices: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async signup(@Body() signupDTO: SignupDto): Promise<User> {
+    return this.userServices.signup(signupDTO);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signin(
+    @Body() signinDTO: SigninDto,
+  ): Promise<{ name: string; email: string; jwtToken: string }> {
+    return this.userServices.signin(signinDTO);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public findAll(): Promise<User[]> {
+    return this.userServices.findAll();
+  }
+}
